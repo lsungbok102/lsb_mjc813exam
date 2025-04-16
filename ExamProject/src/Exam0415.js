08809c35b27f9faf61c9e8762e072225510d09d6
lsungbok102 <lsungbok102@gmail.com>
2025-04-16 19:58:43 +0900

    수정

--- ExamProject/src/Exam0415.js
+++ ExamProject/src/Exam0415.js
@@ -1,6 +1,6 @@
 class Exam0415 {
     pdfExam17() {
-        let height = prompt("키를 입력하세요: ");
+        let height = prompt("키를 입력하세요");
 
         if (height >= 150) {
             console.log("YES");
@@ -10,18 +10,15 @@ class Exam0415 {
     }
 
     pdfExam18(){
-        let input= prompt("점수를 입력하세요: ");
+        let input= prompt("점수를 입력하세요");
         let score = input.split(" ");
         let score_avg = 0;
-        for(let i = 0; i < score.lenth; i++){
+        for(let i = 0; i < score.length; i++){
             score_avg += parseInt(score[i]);
         }
-        score_avg = score_avg / score.lenth;
+        score_avg = score_avg / score.length;
         console.log("평균 점수: ", score_avg);
-
     }
 }
+
 let exam = new Exam0415();
-exam.pdfExam17();
-exam.pdfExam18();
-window.Exam0415 = Exam0415;
