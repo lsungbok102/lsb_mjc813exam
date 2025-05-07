29d2494b8cfaba105749bd662ab9727104cb640e
lsungbok102 <lsungbok102@gmail.com>
2025-05-07 21:33:38 +0900

    5/7

--- CodingTest/src/Solution181867.java
+++ CodingTest/src/Solution181867.java
@@ -7,8 +7,15 @@ public class Solution181867 {
         for (int i = 0; i < a.length; i++) {
             answer[i] = a[i].length();
         }
-
         return answer;
+    }
+    public static void main(String[] args) {
+        Solution181867 sol = new Solution181867();
+        int[] result = sol.solution("oxooxoxxox");;
+        System.out.print("Solution181867 : ");
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
     }
 }
 
